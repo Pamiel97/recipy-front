d8e39c41c635faceb6f25160f59313adc96e13d4
Pamela Milanese <milanesepamela@gmail.com>
2024-12-20 12:38:10 +0100

    aggiunto dei service

--- src/app/model/ingredients/ingredient-service.ts
+++ src/app/model/ingredients/ingredient-service.ts
@@ -1,3 +1,18 @@
-export class IngredientService{
-    
-}
+import { Observable } from "rxjs";
+import { IngredientDto } from "./ingredient-dto";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn: 'root' //DOVE INIETTARLO? 
+  })
+  export class IngredientService {
+    constructor(private http: HttpClient) {} 
+  
+    getIngredientDto(): Observable<IngredientDto[]> {
+      let params = new HttpParams();
+      params = params.set("active", "true");  
+  
+      return this.http.get<IngredientDto[]>("http://localhost:8080/api/ingredients", { params });
+    }
+  }
--- src/app/model/recipes/recipe-service.ts
+++ src/app/model/recipes/recipe-service.ts
@@ -1,7 +1,15 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { RecipeDto } from "./recipe-dto";
+import { Observable } from "rxjs";
 
 export class RecipeService{
-    private urlExtension: string = "/api/recipes"
+    private apiUrl = 'http://localhost:8080/api/recipes';
     constructor(private http: HttpClient){}
     
+
+    createRecipe(recipe: RecipeDto): Observable<RecipeDto> {
+        const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+        return this.http.post<RecipeDto>(this.apiUrl, recipe, { headers });
+    }
+
 }
