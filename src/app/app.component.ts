dbfb2d63430594a3e321ee6606e6721a2f826195
Mirko Risuleo <mirkotm705@gmail.com>
2024-12-23 15:15:17 +0100

    Aggiunge la route e inserisce la home nell'app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Router, RouterModule, RouterOutlet } from '@angular/router';
 import { RecipeAddFormComponent } from './recipes/recipe-add-form/recipe-add-form.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule, HttpClientModule],
+  imports: [RouterOutlet, RouterModule, HttpClientModule, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { RecipeAddFormComponent } from './recipes/recipe-add-form/recipe-add-form.component';
 import { RecipelistComponent } from './recipes/recipelist/recipelist.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     { path: 'create-recipe', component: RecipeAddFormComponent },
     {path: 'search-recipe', component: RecipelistComponent},
+    {path: 'home', component: HomeComponent}
 ];
