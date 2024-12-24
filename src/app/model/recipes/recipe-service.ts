88cfb5d966e3d6d32ef361cbd37d7a8155a3e14b
Pamela Milanese <milanesepamela@gmail.com>
2024-12-24 10:16:13 +0100

    correzione creazione ricette col token

--- src/app/model/recipes/recipe-service.ts
+++ src/app/model/recipes/recipe-service.ts
@@ -11,13 +11,14 @@ import { Injectable } from "@angular/core";
 export class RecipeService{
     private apiUrl = 'http://localhost:8080/api/recipes';
     constructor(private http: HttpClient){}
+    private token = localStorage.getItem('jwtToken');
     
 
-    //Capire come mandare il token senza parssarlo direttamente...
+    
     createRecipe(recipe: RecipeDto): Observable<RecipeDto> {
         const headers = new HttpHeaders({
             'Content-Type': 'application/json',
-            'Authorization': `Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJtaXJrb3Jpc3VsZW9AZ21haWwuY29tIiwiaWF0IjoxNzM0NzcwNzc0LCJleHAiOjE3NjYzMDY3NzR9.pznH8UXKU_N15exNhi6r2WfELDN8k0VyCZIW3EwhX1s`
+            'Authorization': `Bearer ${this.token}`
         });
     
         return this.http.post<RecipeDto>(this.apiUrl, recipe, { headers });
